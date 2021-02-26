adfad2f9ff51937d4144503df327bb65a862354c
excitexcite <excitenew@gmail.com>
2021-02-26 22:24:35 +0200

    Implement bird's ability to fly

---
+++ Assets/Scripts/BirdFly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdFly : MonoBehaviour
+{
+
+    [SerializeField] float velocity = 1;
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody2D.velocity = Vector2.up * velocity;
+        }
+    }
+}
